09321f93b5bc137365d8e990ac26f57cd5f9b0e8
User <User@DESKTOP-0TCDSR6>
2024-08-20 14:49:21 +0300

    Switch background ternary

--- JavaScript02-SelectorsAndEvents/Script2.js
+++ JavaScript02-SelectorsAndEvents/Script2.js
@@ -6,23 +6,24 @@ const LIGHT = "#D8D8D8"
 
 background_switch.onclick = function ()
 {
-	let switch_image = document.getElementById("background_switch");
-	let filename = switch_image.currentSrc.split('/');
-	console.log(filename);
-	if (filename[filename.length - 1] === 'moon.png')
-	{
-		switch_image.src = 'img/sun.png';
-		document.body.className = "dark";
-		//document.body.style.backgroundColor = DARK;
-		//document.body.style.color = LIGHT;
-	}
-	else
-	{
-		switch_image.src = 'img/moon.png';
-		document.body.className = "light";
-		//document.body.style.backgroundColor = LIGHT;
-		//document.body.style.color = DARK;
-	}
+	//let switch_image = document.getElementById("background_switch");
 
-	//document.body.className = document.body.className === "light" ? "dark" : "light";
+	//let filename = switch_image.currentSrc.split('/');
+	//console.log(filename);
+	//if (filename[filename.length - 1] === 'moon.png')
+	//{
+	//	switch_image.src = 'img/sun.png';
+	//	document.body.className = "dark";
+	//	//document.body.style.backgroundColor = DARK;
+	//	//document.body.style.color = LIGHT;
+	//}
+	//else
+	//{
+	//	switch_image.src = 'img/moon.png';
+	//	document.body.className = "light";
+	//	//document.body.style.backgroundColor = LIGHT;
+	//	//document.body.style.color = DARK;
+	//}
+
+	document.body.className = document.body.className === "light" ? "dark" : "light";
 }
