4d31b61fe5a06246da8516ec7c425aa53bd04a59
User <User@DESKTOP-0TCDSR6>
2024-08-24 16:30:48 +0300

    Counstdown timer starts

--- JavaScript02-SelectorsAndEvents/Script2.js
+++ JavaScript02-SelectorsAndEvents/Script2.js
@@ -90,14 +90,14 @@ function animText()
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-let date = new Date();
-let result = "<h3>Дата и время</h3>";
-result += `Полная дата: ${date}<br>`;
-result += `Только дата: ${date.toDateString()}<br>`;
-result += `Только дата: ${date.getFullYear()}.${checkNumber(date.getMonth() + 1)}.${checkNumber(date.getDate())}<br>`;
-result += `День: ${date.getDay()}<br>`;
-result += `Только время: ${date.toTimeString()}<br>`;
-document.getElementById("DateAndTime").innerHTML = result;
+//let date = new Date();
+//let result = "<h3>Дата и время</h3>";
+//result += `Полная дата: ${date}<br>`;
+//result += `Только дата: ${date.toDateString()}<br>`;
+//result += `Только дата: ${date.getFullYear()}.${checkNumber(date.getMonth() + 1)}.${checkNumber(date.getDate())}<br>`;
+//result += `День: ${date.getDay()}<br>`;
+//result += `Только время: ${date.toTimeString()}<br>`;
+//document.getElementById("DateAndTime").innerHTML = result;
 
 document.body.onload = function tick_timer()
 {
@@ -125,6 +125,63 @@ document.body.onload = function tick_timer()
 function checkNumber(i)
 {
 	return i < 10 ? "0" + i : i;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//									Countdown timer
+document.getElementById("btnStart").onclick =
+	function startCountdown()
+	{
+		//DOM - Document Object Model
+		let targetDate = document.getElementById("targetDate")
+		let targetTime = document.getElementById("targetTime")
+		let btnStart = document.getElementById("btnStart");
+		targetDate.disabled = targetTime.disabled = !targetTime.disabled;
+		//btnStart.value = btnStart.value == "Start" ? "Stop" : "Start";
+		if (btnStart.value == "Start")
+		{
+			btnStart.value = "Stop"
+			tickCountdown();
+		}
+		else
+		{
+			btnStart.value = "Start";
+		}
+
+		//let date = document.getElementById("targetDate").valueAsDate;
+		//let time = document.getElementById("targetTime").valueAsDate;
+		//let result = `Дата: ${date.getFullYear()}/${checkNumber(date.getMonth() + 1)}/${checkNumber(date.getDate())}`;
+		//result += `<br>Время:${checkNumber(time.getHours())}:${checkNumber(time.getMinutes())}:${checkNumber(time.getSeconds())}`;
+		//document.getElementById("result").innerHTML = result;
+
+		//let element = document.getElementById("countdownTimer");
+		//let p_date = document.createElement("p");
+		//let p_time = document.createElement("p");
+		////p_date.append(document.createTextNode(`Дата: ${date}`));
+		////p_time.append(document.createTextNode(`Время:${time}`));
+		//p_date.append(document.createTextNode(`Дата: ${date.getFullYear()}/${checkNumber(date.getMonth()+1)}/${checkNumber(date.getDate())}`));
+		//p_time.append(document.createTextNode(`Время:${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`));
+
+		//element.append(p_date);
+		//element.append(p_time);
+	}
+
+function tickCountdown()
+{
+	if (!document.getElementById("targetTime").disabled) return;
+	let now = new Date();
+	let targetTimeControl = document.getElementById("targetTime");
+	let targetTime = targetTimeControl.valueAsDate;
+	//targetTime.setDate(now.toDateString());
+	targetTime.setFullYear(now.getFullYear());
+	targetTime.setMonth(now.getMonth());
+	targetTime.setDate(now.getDate());
+	let duration = targetTime;
+	document.getElementById("result").innerHTML = duration + "<br>" + now;
+	let timestamp = targetTime - now;
+	document.getElementById("result").innerHTML = new Date(timestamp).toTimeString();
+	if (duration > 0) setTimeout(tickCountdown, 1000);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////
