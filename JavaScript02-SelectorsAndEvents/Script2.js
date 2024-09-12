0721a372c1dcee6a6f1ba6723545e1f44626f276
User <User@DESKTOP-0TCDSR6>
2024-09-12 15:25:02 +0300

    Time fixed, but every 100 years we lose one day.

--- JavaScript02-SelectorsAndEvents/Script2.js
+++ JavaScript02-SelectorsAndEvents/Script2.js
@@ -220,16 +220,20 @@ function tickCountdown()
 	console.log(SECONDS_IN_YEAR);
 
 	let timestamp_in_seconds = Math.floor(timestamp / 1000);
+	let time_of_day = timestamp_in_seconds % SECONDS_IN_DAY;
+	timestamp_in_seconds = Math.floor(timestamp_in_seconds / SECONDS_IN_DAY);
+	timestamp_in_seconds = timestamp_in_seconds * SECONDS_IN_DAY;
 
 	let years = Math.floor(timestamp_in_seconds / SECONDS_IN_YEAR);
 	if (years > 0)
 	{
 		timestamp_in_seconds = (timestamp_in_seconds % (years * SECONDS_IN_YEAR));
-		if (document.getElementById("years_unit") == null)
-		{
+		let years_unit = document.getElementById("years_unit");
+		if (years_unit == null) {
 			let display = document.getElementById("display");
 			display.prepend(createTimeBlock("years", years));
 		}
+		else years_unit.innerHTML = years;
 	}
 	else
 	{
@@ -309,13 +313,13 @@ function tickCountdown()
 		removeTimeBlock("days");
 	//console.log(timestamp_in_seconds % days_in_seconds);
 
-	let hours = Math.floor(timestamp_in_seconds / 3600);
-	if (hours > 0) timestamp_in_seconds = (timestamp_in_seconds % (hours * SECONDS_IN_HOUR));
+	let hours = Math.floor(time_of_day / 3600);
+	if (hours > 0) time_of_day = (time_of_day % (hours * SECONDS_IN_HOUR));
 
-	let minutes = Math.floor(timestamp_in_seconds / SECONDS_IN_MINUTE);
-	if (minutes > 0) timestamp_in_seconds = (timestamp_in_seconds % (minutes * 60));
+	let minutes = Math.floor(time_of_day / SECONDS_IN_MINUTE);
+	if (minutes > 0) time_of_day = (time_of_day % (minutes * 60));
 
-	let seconds = Math.floor(timestamp_in_seconds);
+	let seconds = Math.floor(time_of_day);
 
 	//https://planetcalc.com/7741/
 	document.getElementById("result").innerHTML = `${years} Years, ${month} Month, ${weeks} Weeks, ${days} Days, ${checkNumber(hours)} Hours, ${checkNumber(minutes)} Minutes, ${checkNumber(seconds)} Seconds;`;
