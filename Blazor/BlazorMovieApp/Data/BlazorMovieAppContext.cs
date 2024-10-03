13782d3be87c91cc57cc89776006d04f6736f81f
User <User@DESKTOP-0TCDSR6>
2024-10-03 14:44:17 +0300

    CUID Added in MovieApp

---
+++ Blazor/BlazorMovieApp/Data/BlazorMovieAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorMovieApp.Models;
+
+namespace BlazorMovieApp.Data
+{
+    public class BlazorMovieAppContext : DbContext
+    {
+        public BlazorMovieAppContext (DbContextOptions<BlazorMovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorMovieApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
