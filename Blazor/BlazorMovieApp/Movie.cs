26702e97d8e755f877ded74dbc857703ed6b9665
User <User@DESKTOP-0TCDSR6>
2024-10-05 15:53:16 +0300

    CreateMovie delimiters Schmidt

--- Blazor/BlazorMovieApp/Movie.cs
+++ Blazor/BlazorMovieApp/Movie.cs
@@ -6,9 +6,12 @@ namespace BlazorMovieApp.Models;
 public class Movie
 { 
 	public int Id { get; set; }
+	[Required]
 	public string? Title { get; set; }
-	//[DataType(DataType.Date)]
+	[DataType(DataType.Date)]
+	[Range(typeof(DateTime), "1888-10-14", "2025-12-31")]
 	public DateTime? ReleaseDate { get; set; }
 	public string? Genre { get; set; }
+	[Range(150, 1100)]
 	public decimal Price { get; set; }
 }
