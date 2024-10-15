315da13a47c7c132e40e2814b01f73e5ec6b8cf5
User <User@DESKTOP-0TCDSR6>
2024-10-15 16:05:32 +0300

    Disciplines model added

---
+++ Blazor/BlazorAppPD_212/Models/Discipline.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppPD_212.Models
+{
+    public class Discipline
+    {
+        [Key]
+        public int discipline_id { get; set; }
+        [Required]
+        public string discipline_name { get; set; }
+        [Required]
+        public int number_of_lessons { get; set; }
+    }
+}
---
+++ Blazor/BlazorAppPD_212/Models/TeachersDisciplinesRelation.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppPD_212.Models
+{
+	public class TeachersDisciplinesRelation
+	{
+		[Key]public int teacher { get; set; }
+		[Key]public short discipline { get; set; }
+	}
+}
