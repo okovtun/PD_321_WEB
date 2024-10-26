c5675e0d030fa4a5a7f11a87de83ec08d34afe87
User <User@DESKTOP-0TCDSR6>
2024-10-26 16:48:24 +0300

    DB Initialize in Contoso University

---
+++ ASP/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+	public class Course
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int CourseId { get; set; }
+		public string Title {  get; set; }
+		public int Credits { get; set; }
+
+		//Navigation property:
+		public ICollection<Enrollment> Enrollments { get; set; }
+	}
+}
---
+++ ASP/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace ContosoUniversity.Models
+{
+	public class Student
+	{
+		public int ID { get; set; }
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public DateTime EnrollmentDate { get; set; }
+
+		//Navigation property:
+		public ICollection<Enrollment> Enrollments { get; set; }
+	}
+}
