7bc42f41f08cb46728694d97ddb8074f75e47311
User <User@DESKTOP-0TCDSR6>
2024-10-29 16:31:01 +0300

    Search filter

--- ASP/ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ASP/ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -10,20 +10,54 @@ using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Pages.Students
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ContosoUniversity.Data.SchoolContext _context;
+	public class IndexModel : PageModel
+	{
+		private readonly ContosoUniversity.Data.SchoolContext _context;
 
-        public IndexModel(ContosoUniversity.Data.SchoolContext context)
-        {
-            _context = context;
-        }
+		public IndexModel(ContosoUniversity.Data.SchoolContext context)
+		{
+			_context = context;
+		}
+		public string LastNameSort { get; set; }
+		public string FirstNameSort { get; set; }
+		public string DateSort { get; set; }
+		public string CurrentFilter { get; set; }
+		public string CurrentSort { get; set; }
 
-        public IList<Student> Student { get;set; } = default!;
+		public IList<Student> Students { get; set; } = default!;
 
-        public async Task OnGetAsync()
-        {
-            Student = await _context.Students.ToListAsync();
-        }
-    }
+		public async Task OnGetAsync(string sortOrder, string searchString)
+		{
+			LastNameSort = String.IsNullOrEmpty(sortOrder) ? "last_name_desc" : "";  //Descinding by Name
+			FirstNameSort = sortOrder == "first_name_asc" ? "firs_name_desc" : "first_name_asc";
+			DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+			CurrentFilter = searchString;
+
+			IQueryable<Student> students = from s in _context.Students select s;
+
+			if (!String.IsNullOrEmpty(searchString))
+			{
+				students = students.Where
+					(
+							s => s.LastName.Contains(searchString)
+						|| s.FirstName.Contains(searchString)
+					);
+			}
+
+			switch (sortOrder)
+			{
+				default:			students = students.OrderBy(s => s.LastName);					break;
+				case "last_name_desc":	students = students.OrderByDescending(s => s.LastName);		break;
+				
+				case "first_name_asc":	students = students.OrderBy(s => s.FirstName);				break;
+				case "first_name_desc":	students = students.OrderByDescending(s => s.FirstName);	break;
+				
+				case "Date":		students = students.OrderBy(s => s.EnrollmentDate);				break;
+				case "dete_desc":	students = students.OrderByDescending(s => s.EnrollmentDate);	break;
+			}
+
+			Students = await students.AsNoTracking().ToListAsync();
+		}
+	}
 }
