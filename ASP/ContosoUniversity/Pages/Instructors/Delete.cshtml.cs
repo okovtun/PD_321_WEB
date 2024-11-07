679c4c38eb868d9d109ff0d8aefcf484b7df052b
User <User@DESKTOP-0TCDSR6>
2024-11-07 16:13:01 +0300

    OffceAssignment & Courses shown on 'Delete' page

--- ASP/ContosoUniversity/Pages/Instructors/Delete.cshtml.cs
+++ ASP/ContosoUniversity/Pages/Instructors/Delete.cshtml.cs
@@ -10,39 +10,40 @@ using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Pages.Instructors
 {
-    public class DeleteModel : PageModel
-    {
-        private readonly ContosoUniversity.Data.SchoolContext _context;
+	public class DeleteModel : InstructorCoursesPageModel
+	{
+		private readonly ContosoUniversity.Data.SchoolContext _context;
 
-        public DeleteModel(ContosoUniversity.Data.SchoolContext context)
-        {
-            _context = context;
-        }
+		public DeleteModel(ContosoUniversity.Data.SchoolContext context)
+		{
+			_context = context;
+		}
 
-        [BindProperty]
-        public Instructor Instructor { get; set; } = default!;
+		[BindProperty]
+		public Instructor Instructor { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var instructor = await _context.Instructors.FirstOrDefaultAsync(m => m.ID == id);
+			var instructor = await _context.Instructors.Include(i => i.OfficeAssignment).Include(i => i.Courses).FirstOrDefaultAsync(m => m.ID == id);
 
-            if (instructor == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                Instructor = instructor;
-            }
-            return Page();
-        }
+			if (instructor == null)
+			{
+				return NotFound();
+			}
+			else
+			{
+				Instructor = instructor;
+			}
+			PopulateAssignedCourseData(_context, Instructor);
+			return Page();
+		}
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+		/*public async Task<IActionResult> OnPostAsync(int? id)
         {
             if (id == null)
             {
@@ -58,6 +59,24 @@ namespace ContosoUniversity.Pages.Instructors
             }
 
             return RedirectToPage("./Index");
-        }
-    }
+        }*/
+
+		public async Task<IActionResult> OnPostAsync(int? id)
+		{
+			if (id == null) return NotFound();
+
+			Instructor instructor = await _context.Instructors
+				.Include(i => i.OfficeAssignment)
+				.Include(i => i.Courses)
+				.SingleAsync(i => i.ID == id);
+
+			if (instructor == null) return RedirectToPage("./Index");
+			List<Department> departments = await _context.Departments.Where(d => d.InstructorID == id).ToListAsync();
+			departments.ForEach(d => d.InstructorID = null);
+
+			_context.Instructors.Remove(instructor);
+			await _context.SaveChangesAsync();
+			return RedirectToPage("./Index");
+		}
+	}
 }
