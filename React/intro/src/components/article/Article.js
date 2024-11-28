4cb7cd3261dbcecffe8300867d5f23d28798c683
User <User@DESKTOP-0TCDSR6>
2024-11-28 16:11:51 +0300

    db.json & icons

--- React/intro/src/components/article/Article.js
+++ React/intro/src/components/article/Article.js
@@ -2,12 +2,13 @@ function Article()
 {
     return(
             <article>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, fuga. Explicabo tenetur voluptas, velit, quae aliquam sint saepe sit fugiat tempora maxime adipisci? Quo quam voluptatum dolor eum, velit optio.</p>
+                
+            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, fuga. Explicabo tenetur voluptas, velit, quae aliquam sint saepe sit fugiat tempora maxime adipisci? Quo quam voluptatum dolor eum, velit optio.</p>
             <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut quae molestias sed. Molestias facere repellendus provident velit vel, minus, quisquam totam placeat consequatur assumenda consequuntur cumque sequi corporis tempore earum.
             Cupiditate qui facere nesciunt, saepe molestiae doloribus veniam. Laborum magnam nam non harum. Corrupti, officiis sequi sint velit saepe porro. Id maxime voluptas magnam labore doloribus rem incidunt laboriosam ducimus.
             Ipsum alias nulla voluptatem corrupti. Et quam deleniti culpa modi numquam facilis enim praesentium, dolorem ea ex corporis tenetur qui nesciunt est velit aut soluta exercitationem asperiores laudantium tempore temporibus!
-            </p>
+            </p> */}
         </article>
     );
 }
