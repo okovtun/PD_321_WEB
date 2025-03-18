b00b73699d32ff5fe47170300df049532c3aeeec
User <User@DESKTOP-0TCDSR6>
2025-03-18 13:40:07 +0300

    SSMS

--- PHP/DataBase/PD_321_PHP/Teachers/SQLQuery2 - SELECT Teachers for iscipline.sql
+++ PHP/DataBase/PD_321_PHP/Teachers/SQLQuery2 - SELECT Teachers for iscipline.sql
@@ -5,7 +5,8 @@ SELECT
 		teacher_id,
 		FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name) AS 'full_name',
 		birth_date,
-		DATEDIFF(DAY, work_since, GETDATE())/365			AS 'experience'
+		DATEDIFF(DAY, work_since, GETDATE())/365			AS 'experience',
+		discipline_name
 FROM	Disciplines
 JOIN	TeachersDisciplinesRelation	ON	(discipline=discipline_id)
 JOIN	Teachers					ON	(teacher=teacher_id)
